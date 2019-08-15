460b3dbb74d78bf3ba090f99f12c231efcccfff5
Jubaer Alnazi <51982912+jubaer-binary@users.noreply.github.com>
2019-08-15 17:55:17 +0800

    Update binary-eye.sh

--- binary-eye.sh
+++ binary-eye.sh
@@ -1,8 +1,28 @@
-
+#!/bin/bash
+#Colors
+RED=$(echo -en '\001\033[00;31m\002')
+GREEN=$(echo -en '\001\033[00;32m\002')
+WHITE=$(echo -en '\001\033[01;37m\002')
+echo"${WHITE}
 ██████╗ ██╗███╗   ██╗ █████╗ ██████╗ ██╗   ██╗    ███████╗██╗   ██╗███████╗
 ██╔══██╗██║████╗  ██║██╔══██╗██╔══██╗╚██╗ ██╔╝    ██╔════╝╚██╗ ██╔╝██╔════╝
 ██████╔╝██║██╔██╗ ██║███████║██████╔╝ ╚████╔╝     █████╗   ╚████╔╝ █████╗  
 ██╔══██╗██║██║╚██╗██║██╔══██║██╔══██╗  ╚██╔╝      ██╔══╝    ╚██╔╝  ██╔══╝  
 ██████╔╝██║██║ ╚████║██║  ██║██║  ██║   ██║       ███████╗   ██║   ███████╗
 ╚═════╝ ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚══════╝   ╚═╝   ╚══════╝
-                                                                           
+Enter Domain-"
+read domains
+while true
+do
+echo "${GREEN}...................................................................."
+echo "${WHITE}Domain: ${GREEN}$domains"
+NMAP=$(nmap $domains | GREP_COLORS='mt=01;32' egrep --color=always 'open|\closed')
+sleep 10
+NMAP2=$(nmap $domains | GREP_COLORS='mt=01;32' egrep --color=always 'open|\closed')
+echo "${GREEN}...................................................................."
+if [ "$NMAP" == "$NMAP2" ]; then
+    echo "${GREEN}Seems Like No Ports Have Been Open!"
+else
+    echo "${RED}ALERT! A NEW PORT WAS DETECTED"
+fi
+done

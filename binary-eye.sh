7e31761927dfc626d3a4aac3b5094142c5027089
Jubaer Alnazi <51982912+jubaer-binary@users.noreply.github.com>
2019-09-19 13:04:35 +0800

    Update binary-eye.sh

--- binary-eye.sh
+++ binary-eye.sh
@@ -1,82 +1,14 @@
-#!/bin/bash
-#Developer : Jubaer Alnazi
-#Colors
-RED=$(echo -en '\001\033[00;31m\002')
-GREEN=$(echo -en '\001\033[00;32m\002')
-WHITE=$(echo -en '\001\033[01;37m\002')
+logfile="scan_$(date +%Y%m%d_%H%M%S).log"
+masscan -p0-65535 --max-rate 10000 -Pn -vv -iL ../hosts.txt | grep "Discovered open port" | awk {'print $6":"$4'} | awk -F/ {'print $1'}| sort -u >> /root/BinaryEye/$logfile
 
-echo "
-${RED}
-  .  . .  .  . .  . . . . . . . . . .  .  . .  .
-   .       .                          .       .
-     .  .    .  . . . . . . .  .  .     . .     .
- .       .          .;tSSX%%:.   .  .       .
-   .  .    .  :@tSX t8888888@%; ;t@. . .  .   . .
-  .    .  . :t @@8@@S8@tSS@@8S88@@8 S:      .
-    .    .8@:888t %;888888X@@8;X ;888:88: .    .
-  .   .;: 8@8  8t:@S88888@8@8SXX.:8: 8@8 :: .    .
- .    %X888.S   S8@88888@8888@@88S   X.888S%  .
-   ..S.@8tt%  :@@8888;@888;@8888@8@:  %:;X8.S.  .
- . ;888@%%   t@8888@8S%888SX888@88.@t  .t%@@88;
-  .8888;  . .%%888@X.  t8: 8;S88@888 .    ;8888. .
- .:888..    %88888X%. .@88S%8@:@888888:   ..888:.
- %8@@8    . @@8888XtS X@@888@@tX888@88:     8888%
- .888X  .   8:88@8S:88888888@@:%8;@8   .    X@88.
- S 8@ %     888;X888tS@88888XSS88@8888   . %.8@ S
-  S 88 8 . .tX8@888@88t .: t@8888888@%    8.88 %
- . S @8;8  .:88888S88S8:.:.XX88@;@888. . 8SX8.X .
-    8 8@S.; .:88888888888@888@88X888%  ;:XS8 @
-   .  :;X8SSX:t8X88 @8@8888@88888 8t X%@@@;..   .
- .    .SX@X8X%t.:88888;X88@88:S 8..t 8@88XS   .
-        :S X@8SX 8S%8X8XXS %8;%t8 88888 @:      .
-  . .  .   t;@t8888X.88@tS@8StX888@%X:t     . .
-             .tX8S8@8888888@@@@8SS%;            .
-  .  .      .  ..t@X.;X8888X%.%8S .  .    . .
-   .   .  .           .        .   .    .     .  .
-     .   .   .  . .  .      .         .    .   .
- .     .   .        .    .   . .  .      .   .
-   .         . .  .   . .  .  .  .  .  .   .    .
+old="$(ls /root/BinaryEye -lt | grep nmap_tcp__scan | awk 'NR==2,NR==2 {print $9}')"
+new="$(ls /root/BinaryEye -lt | grep nmap_tcp__scan | awk 'NR==1,NR==1 {print $9}')"
 
-${GREEN}
- ____   ____  ____    ____  ____   __ __        ___  __ __    ___
-|    \ |    ||    \  /    ||    \ |  |  |      /  _]|  |  |  /  _]
-|  o  ) |  | |  _  ||  o  ||  D  )|  |  |     /  [_ |  |  | /  [_
-|     | |  | |  |  ||     ||    / |  ~  |    |    _]|  ~  ||    _]
-|  O  | |  | |  |  ||  _  ||    \ |___, |    |   [_ |___, ||   [_
-|     | |  | |  |  ||  |  ||  .  \|     |    |     ||     ||     |
-|_____||____||__|__||__|__||__|\_||____/     |_____||____/ |_____|
+res="$(printf "New ports \n" && printf "\n\n\n\n" &&  comm -3  /root/BinaryEye/$old  /root/BinaryEye/$new |pcregrep -r "\t" && printf "\n\n\n\n" && echo -e  "Closed ports \n" && comm -3  /root/BinaryEye/$old  /root/BinaryEye/$new |pcregrep -v -r "\t")"
+
+sendAlert() {
+sendemail -o tls=yes -f "alert@binaryeye.com" -t "alertmail@mail.com" -s "smtp.sendgrid.net:587" -xu apikey -xp "<KEY>" -u "Alerts For Port Activity From Binary Eye" -m "$res"
+}
 
 
-
-${WHITE}Enter Domain"
-read domains
-echo "${WHITE}From Email:"
-read frommail
-echo "${WHITE}To Email:"
-read tomail
-echo "${WHITE}Gmail Username:"
-read gmailusername
-echo "${WHITE}Gmail Password:"
-read gmailpassword
-
-
-
-echo "${GREEN}...................................................................."
-echo "${WHITE}Domain: ${GREEN}$domains"
-NMAP=$(nmap -p- $domains | GREP_COLORS='mt=01;32' egrep --color=always 'open|\closed')
-sleep 5
-while true
-do
-NMAP2=$(nmap -Pn -p- $domains | GREP_COLORS='mt=01;32' egrep --color=always 'open|\closed')
-echo "${GREEN}...................................................................."
-if [ "$NMAP" == "$NMAP2" ]; then
-    echo "${GREEN}Seems Like No Ports Have Been Opened!"
-    
- else
-    echo "${RED}ALERT! A NEW PORT ACTIVITY WAS DETECTED" | sendemail -o tls=yes -f $frommail -t $tomail -s smtp.gmail.com:587 -xu $gmailusername -xp $gmailpassword -u "ALERT - NEW PORT ACTIVITY DETECTED $domains" -m "New Port list-
-$NMAP2"
-exit 1
-fi
-done
-
- 
+if [[ $(comm -3 /root/BinaryEye/$old /root/BinaryEye/$new) ]]; then sendAlert ; fi
